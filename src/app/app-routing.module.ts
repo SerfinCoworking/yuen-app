45b8924fdda1358b0ecb124177373d579effabc8
paul <paul.18.sma@gmail.com>
2021-08-28 21:56:30 -0300

    removes unused modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,48 +9,6 @@ const routes: Routes = [
 		canActivate: [AuthGuard],
 		loadChildren: () =>
 			import("@modules/home/home.module").then((m) => m.HomeModule)
-	},
-	{
-		path: "usuarios",
-		canActivate: [AuthGuard],
-		loadChildren: () =>
-			import("@modules/user/user.module").then((m) => m.UserModule)
-	},
-	{
-		path: "cuentas",
-		loadChildren: () =>
-			import("@modules/accounts/accounts.module").then((m) => m.AccountsModule)
-	},
-	{
-		path: "productos",
-		loadChildren: () =>
-			import("@modules/products/products.module").then((m) => m.ProductsModule)
-	},
-	{
-		path: "proveedores",
-		loadChildren: () =>
-			import("@modules/providers/providers.module").then(
-				(m) => m.ProvidersModule
-			)
-	},
-	{
-		path: "clientes",
-		loadChildren: () =>
-			import("@modules/clients/clients.module").then((m) => m.ClientsModule)
-	},
-	{
-		path: "empresas",
-		loadChildren: () =>
-			import("@modules/companies/companies.module").then(
-				(m) => m.CompaniesModule
-			)
-	},
-	{
-		path: "compras",
-		loadChildren: () =>
-			import("@modules/purchases/purchases.module").then(
-				(m) => m.PurchasesModule
-			)
 	}
 ];
 
